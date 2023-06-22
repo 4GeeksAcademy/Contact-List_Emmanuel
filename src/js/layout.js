e8d932948ff98059bec51f77f847951aa5019a32
Emmanuel Vargas <126733106+EmmanuelV22@users.noreply.github.com>
2023-06-22 03:22:55 +0000

    Done

--- src/js/layout.js
+++ src/js/layout.js
@@ -2,8 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 
-import { Home } from "./views/home";
-import { Demo } from "./views/demo";
+
 
 import ContactList from "./views/ContactList.jsx";
 import injectContext from "./store/appContext";
